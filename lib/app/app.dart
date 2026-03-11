515c26e3ba1769aa2c26e05facaead88c75be9c5
tif1336 <l200230159@student.ums.ac.id>
2026-03-12 01:08:51 +0700

    fix: hapus unused import splash page di app.dart

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:nalargizi/features/dashboard/presentation/pages/dashboard_page.dart';
 
 import 'theme/app_theme.dart';
-import '../features/splash/presentation/pages/splash_page.dart';
 
 class NalarGiziApp extends StatelessWidget {
   const NalarGiziApp({super.key});
